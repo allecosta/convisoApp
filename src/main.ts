43384a7cce53197847ff5ac7fd0d8f3d298e2f5f
allecosta <alle_costa10@outlook.com>
2025-05-12 15:21:31 -0300

    First commit

---
+++ src/main.ts
@@ -0,0 +1,35 @@
+const express = require('express');
+const mariadb = require('mariadb');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// credenciais apenas para testes 
+const pool = mariadb.createPool({
+  host: 'mariadb',
+  port: 3306,
+  user: 'admin',
+  password: '123',
+  database: 'convisoDB',
+  connectionLimit: 5
+});
+
+app.get('/', async (req, res) => {
+  let conn;
+  try {
+    conn = await pool.getConnection();
+    const rows = await conn.query("SELECT 'Conexão bem-sucedida com MariaDB!' AS mensagem");
+    res.json({ status: "OK", message: rows[0].mensagem });
+  } catch (err) {
+    console.error("Erro ao conectar ao banco:", err);
+    res.status(500).json({ status: "ERRO", error: err.message });
+  } finally {
+    if (conn) conn.release();
+  }
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
+
